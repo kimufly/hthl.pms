278d20eb93b17d075253cae9c59e38664f7975e6
LJJ <linjingjing@hthl-tech.com>
2019-08-16 16:54:31 +0800

    登录页面

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,7 +254,8 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  # config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
@@ -296,4 +297,9 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+end
+
+Rails.application.config.to_prepare do
+  Devise::SessionsController.layout  "devise"
+  # And/or Sessions, Confirmations, Unlocks, Passwords
 end
