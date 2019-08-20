7216a2685151df528d4451e00d10c1820f0c0a7f
xin <1148801025@qq.com>
2019-08-20 19:27:55 +0800

    用户管理 角色管理 客户管理页面提交

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  
+  get 'clients/index'
+  get 'roles/index'
   get 'home/index'
   root 'hello#index'
   devise_for :users, controllers: {
       sessions: 'users/sessions'
   }
+  resources :users 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
