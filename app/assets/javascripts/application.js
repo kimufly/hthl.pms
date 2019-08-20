d3bbb1f2763a8fcc3fc3c759169fe3c8da37e72b
xin <1148801025@qq.com>
2019-08-20 16:44:44 +0800

    主页代码提交

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require_tree ./bootstrap-datepicker
+//= require_tree ./bootstrap-datepicker/locales
 //= require_tree .
