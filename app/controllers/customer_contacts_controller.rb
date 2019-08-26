4859ef0d260d68f47e873b2bc860689998826e54
xin <1148801025@qq.com>
2019-08-26 20:38:50 +0800

    客户模块页面修改

--- app/controllers/customer_contacts_controller.rb
+++ app/controllers/customer_contacts_controller.rb
@@ -3,11 +3,53 @@ class CustomerContactsController < ApplicationController
     @customer_contacts = CustomerContact.all
   end
 
-  def new
-    @customer = CustomerContact.new
+  def show
+    @customer_contact = CustomerContact.find(params[:id])
   end
 
-  private
-  def contact_params
+
+  def new
+    @customer_contact = CustomerContact.new
   end
+
+  def edit
+    @customer_contact = CustomerContact.find(params[:id])
+  end
+
+  def create
+    @customer_contact = CustomerContact.new(customer_params)
+    #todo.....保存父类返回
+    @customer_contact.customer_id=2
+    if @customer_contact.save
+      redirect_to customer_contacts_path
+    else
+      redirect_to "new"
+    end
+  end
+
+  def update
+    @customer_contact = CustomerContact.find(params[:id])
+    if @customer_contact.update(customer_params)
+      redirect_to customer_contacts_path
+    else
+      redirect_to "edit"
+    end
+  end
+
+  def destroy
+    @customer_contact = CustomerContact.find(params[:id])
+    @customer_contact.destroy
+    redirect_to customers_path
+  end
+
+
+
+
+  private
+  def customer_params
+      params.require(:customer_contact).permit(:unit_name, :project_name, :name, :telephone, :phone_number, :other_phone, :position, :email, :address, :customer_id)
+  end
+
+
+
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,22 +1,6 @@
 class CustomersController < ApplicationController
 
-  def create
-    @customerContact = CustomerContact.new(customer_contact_params)
-    print("进入接口方法11111111111111111111111111.\n\n\n\n\n\n\n")
-    if @customerContact.save
-      redirect_to customers_path
-    else
-      redirect_to  new_customer_customers_path
-    end
-  end
 
-  def new_customer
-  end
-
-  private
-  def customer_contact_params
-      params.require(:customer_contact).permit(:unit_name, :project_name, :name, :telephone, :phone_number, :other_phone, :position, :email, :address)
-  end
 
 
 
