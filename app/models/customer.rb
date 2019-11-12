3b0aea8a4f369dd4ab99e41d86d64fbe70064347
206502914 <1148801025@qq.com>
2019-11-12 16:06:47 +0800

    user模块bug修改

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :customer_contacts
+  has_many :customer_contacts, :dependent => :destroy
   has_many :projects
   accepts_nested_attributes_for :customer_contacts
 end
