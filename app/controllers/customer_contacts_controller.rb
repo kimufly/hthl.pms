c415c9536755ac2a458f127f5ea680d935a23a44
206502914 <1148801025@qq.com>
2019-11-20 10:11:27 +0800

    project bug修改

--- app/controllers/customer_contacts_controller.rb
+++ app/controllers/customer_contacts_controller.rb
@@ -57,8 +57,7 @@ class CustomerContactsController < ApplicationController
 
   def destroy
     @customer_contact = CustomerContact.find(params[:id])
-    byebug
-    #@customer_contact.destroy
+    @customer_contact.destroy
     redirect_to customer_contacts_path
   end
 
